55bf1bea588d685c482974ecda1211008974421f
Gustavo Silva <gustavodev60@gmail.com>
2024-07-01 20:20:00 -0300

    create usecase interfaces

--- application/src/main/java/com/gustavo/admin/catalogo/application/UseCase.java
+++ application/src/main/java/com/gustavo/admin/catalogo/application/UseCase.java
@@ -1,9 +1,8 @@
 package com.gustavo.admin.catalogo.application;
 
-import com.gustavo.admin.catalogo.domain.category.Category;
 
-public class UseCase {
-    public Category execute() {
-        return new Category();
-    }
+
+public abstract class UseCase<IN,OUT> {
+
+    public abstract OUT execute(IN input);
 }
