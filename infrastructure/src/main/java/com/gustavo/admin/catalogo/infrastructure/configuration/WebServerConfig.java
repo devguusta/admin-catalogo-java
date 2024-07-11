35223a4a3196c6450988b5fb69549f5bda4534a6
Gustavo Silva <gustavodev60@gmail.com>
2024-07-11 18:05:03 -0300

    config docker and application

---
+++ infrastructure/src/main/java/com/gustavo/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.gustavo.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.gustavo.admin.catalogo")
+public class WebServerConfig {
+}
